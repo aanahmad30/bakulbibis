9e9420a75eeb801ab8fc006c87b5d0057a731c47
aanahmad30 <110350938+aanahmad30@users.noreply.github.com>
2024-12-16 16:37:31 +0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import BestSeller from '../components/BestSeller'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
-import NewsLetterBox from '../components/NewsLetterBox.jsx'
+import NewsLetterBox from '../components/NewsLetterBox'
 import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
