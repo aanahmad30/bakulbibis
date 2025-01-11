37864ad289ed619fe266f84f547109dff7c15a0b
aanahmad30 <110350938+aanahmad30@users.noreply.github.com>
2025-01-11 18:03:05 +0700

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -39,6 +39,7 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
+import qris_image from './qris_image.png';
 
 export const assets = {
     logo,
@@ -58,7 +59,8 @@ export const assets = {
     contact_img,
     razorpay_logo,
     stripe_logo,
-    cross_icon
+    cross_icon,
+    qris_image
 }
 
 
