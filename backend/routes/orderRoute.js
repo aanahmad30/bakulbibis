2d0324c00c73fa99c4617e9c66ea33159320fafd
aanahmad30 <110350938+aanahmad30@users.noreply.github.com>
2025-01-11 19:55:02 +0700

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,20 +1,19 @@
-import express from 'express'
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus} from '../controllers/ordersController.js'
-import adminAuth from '../middleware/adminAuth.js'
-import authUser from '../middleware/auth.js'
+import express from 'express';
+import { placeOrder, allOrders, userOrders, updateStatus, confirmQrisPayment } from '../controllers/ordersController.js';
+import adminAuth from '../middleware/adminAuth.js';
+import authUser from '../middleware/auth.js';
 
-const orderRouter = express.Router()
+const orderRouter = express.Router();
 
 // Admin Features
-orderRouter.post('/list',adminAuth,allOrders)
-orderRouter.post('/status',adminAuth,updateStatus)
+orderRouter.post('/list', adminAuth, allOrders);  // Melihat semua pesanan (Admin)
+orderRouter.post('/status', adminAuth, updateStatus);  // Mengupdate status pesanan (Admin)
 
 // Payment Features
-orderRouter.post('/place',authUser,placeOrder)
-orderRouter.post('/stripe',authUser,placeOrderStripe)
-orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
+orderRouter.post('/place', authUser, placeOrder);  // Menempatkan pesanan dengan metode pembayaran
+orderRouter.post('/qrispayment', authUser, confirmQrisPayment);  // Mengonfirmasi pembayaran QRIS
 
 // User Feature
-orderRouter.post('/userorders',authUser,userOrders)
+orderRouter.post('/userorders', authUser, userOrders);  // Melihat pesanan pengguna
 
-export default orderRouter
+export default orderRouter;
