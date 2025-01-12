2d959e0674a8a62edfbc0973b07c1c6f2c75aab2
aanahmad30 <110350938+aanahmad30@users.noreply.github.com>
2025-01-12 11:55:15 +0700

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -68,7 +68,7 @@ const Cart = () => {
         <div className='w-full sm:w-[450px]'>
           <CartTotal />
           <div className='w-full text-end'>
-            <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCCED TO CHECKOUT</button>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>ORDER SEKARANG</button>
           </div>
         </div>
       </div>
