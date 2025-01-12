bd252c004694e60b5db073e220c41ecc2f2864c4
aanahmad30 <110350938+aanahmad30@users.noreply.github.com>
2025-01-12 12:01:31 +0700

    Update LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -15,23 +15,25 @@ const LatestCollection = () => {
 
   return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Title text1={'PRODUK'} text2={'KULINER'} />
-            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-                UMKM Padukuhan Bibis menyediakan berbagai macam Makanan dan Minuman Khas Padukuhan Bibis.
-            </p>
-        </div>
-
-        {/* Rendering Products */}
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
-            {
-                LatestProducts.map((item,index)=>(
-                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
-                ))
-            }
-        </div>
-
+  <div className='text-center py-8'>
+    <div className='text-3xl font-bold flex flex-col items-center'>
+      <Title text1={'PRODUK'} text2={'KULINER'} />
     </div>
+    <p className='w-full max-w-md m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+      UMKM Padukuhan Bibis menyediakan berbagai macam Makanan dan Minuman Khas Padukuhan Bibis.
+    </p>
+  </div>
+
+  {/* Rendering Products */}
+  <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+    {
+      LatestProducts.map((item, index) => (
+        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+      ))
+    }
+  </div>
+</div>
+
   )
 }
 
