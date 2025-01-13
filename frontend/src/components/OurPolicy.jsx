81b1371ccafd07e07f38b4edba501c24aadb7828
aanahmad30 <110350938+aanahmad30@users.noreply.github.com>
2025-01-13 15:23:03 +0700

    Update OurPolicy.jsx

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -8,18 +8,18 @@ const OurPolicy = () => {
             
             <div>
                 <img src={assets.exchange_icon} className='w-12 m-auto mb-5 ' alt="" />
-                <p className='font-semibold' >Easy Exchange Policy</p>
-                <p className='text-gray-400' >We offer hassle free exchange poilcy</p>
+                <p className='font-semibold' >Pengembalian Produk</p>
+                <p className='text-gray-400' >Kebijakan pengembalian produk</p>
             </div>
             <div>
                 <img src={assets.quality_icon} className='w-12 m-auto mb-5 ' alt="" />
-                <p className='font-semibold' >7 Days Return Policy</p>
-                <p className='text-gray-400' >We provide 7 days free return policy</p>
+                <p className='font-semibold' >Produk Berkualitas</p>
+                <p className='text-gray-400' >Setiap produk dibuat dengan standar terbaik</p>
             </div>
             <div>
                 <img src={assets.support_img} className='w-12 m-auto mb-5 ' alt="" />
-                <p className='font-semibold' >Best customer support</p>
-                <p className='text-gray-400' >We provide 24/7 customer support</p>
+                <p className='font-semibold' >Layanan Pelanggan 24/7</p>
+                <p className='text-gray-400' >Tim kami siap membantu kapan saja</p>
             </div>
 
         </div>
