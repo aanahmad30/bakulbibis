0e27327ff557f3791aa92c0958a8d55bade5c78a
aanahmad30 <110350938+aanahmad30@users.noreply.github.com>
2025-01-13 15:22:41 +0700

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,22 +9,21 @@ const Footer = () => {
             <div>
                 <img src={assets.logo} className='mb-5 w-32' alt="" />
                 <p className='w-full md:w-2/3 text-gray-600'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, vel ratione. Veniam vel et accusantium consectetur odio, tempore sapiente nisi.
+                    Kami berkomitmen untuk menyediakan berbagai macam produk mulai dari f&b, fashion, dll. Kami hadir untuk mempermudah pembelian dari mana saja.
                 </p>
             </div>
 
             <div>
-                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <p className='text-xl font-medium mb-5'>LAYANAN</p>
                 <ul className='flex flex-col gap-1 text-gray-600'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Delivery</li>
-                    <li>Privacy policy</li>
+                    <li><a href="/collection" className="hover:text-black transition">Produk</a></li>
+                    <li><a href="/about" className="hover:text-black transition">Tentang Kami</a></li>
+                    <li><a href="/contact" className="hover:text-black transition">Kontak Kami</a></li>
                 </ul>
             </div>
 
             <div>
-                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <p className='text-xl font-medium mb-5'>HUBUNGI KAMI</p>
                 <ul className='flex flex-col gap-1 text-gray-600'>
                     <li>083866151200</li>
                     <li>contact@gmail.com</li>
